a4c1ea3d19df43a0f06e0591f532e8f0d4dbf45a
Guo Yunhe <i@guoyunhe.me>
2024-02-03 20:57:19 +0800

    lint

--- src/private/deleteMerge.ts
+++ src/private/deleteMerge.ts
@@ -1,10 +1,5 @@
 export function deleteMerge(target: any, source: any) {
-  if (
-    source &&
-    typeof source === 'object' &&
-    target &&
-    typeof target === 'object'
-  ) {
+  if (source && typeof source === 'object' && target && typeof target === 'object') {
     Object.keys(source).forEach((key) => {
       if (source[key]) {
         deleteMerge(target[key], source[key]);
